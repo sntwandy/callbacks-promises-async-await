bdb10ae6912c8e8f837a441a5526130a7639657d
sntwandy <sntwandy@gmail.com>
2020-07-04 15:00:46 -0400

    Callback hell

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import './styles.css';
 
 
 
-const heroId = 'meg';
+const heroId1 = 'meg';
+const heroId2 = 'zeus';
 
-searchHero(heroId , (err, hero) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.info(hero);
-    };
+searchHero(heroId1 , (err, hero1) => {
+
+    if (err) { console.error(err); }
+
+    searchHero(heroId2, (err, hero2) => {
+        if (err) { console.error(err); }
+
+        console.log(`Sending to ${hero1.name} and ${hero2.name}`);
+    })
 });
