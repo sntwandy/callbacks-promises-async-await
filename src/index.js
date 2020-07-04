a55132263040989f109aaf488061f47da4bb52d3
sntwandy <sntwandy@gmail.com>
2020-07-04 15:26:39 -0400

    Using Promise.all

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './styles.css';
 
 
 const heroId1 = 'meg';
-const heroId2 = 'zeus';
+const heroId2 = 'darcy';
 
 /* searchHero(heroId1 , (err, hero1) => {
 
@@ -19,6 +19,15 @@ const heroId2 = 'zeus';
     })
 }); */
 
-searchHero(heroId1)
-    .then(hero => console.log(`Sending to ${hero.name} to the mission!`))
-    .catch();
+/* searchHero(heroId1)
+    .then(hero1 => {
+        //console.log(`Sending to ${hero.name} to the mission!`)
+        searchHero(heroId2)
+            .then(hero2 => console.log(`Sending ${hero1.name} and ${hero2.name} to the mission!`))
+    })
+    .catch(); */
+
+Promise.all([searchHero(heroId1), searchHero(heroId2)])
+    .then( ([hero1, hero2]) => console.log(`Sending ${hero1.name} and ${hero2.name} to the mission!`));
+
+console.warn('Finished:)');
